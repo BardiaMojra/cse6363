57680ac0cbf7deedda78f28c690a1eceb49741e1
bardi <bardia.mojra@gmail.com>
2021-03-11 18:26:14 -0600

    Add proj 01 material.

--- projects/proj01/p01/logisticReg.py
+++ projects/proj01/p01/logisticReg.py
@@ -1,15 +1,29 @@
-W = 'W'
-M = 'M'
+W = 0
+M = 1
 
 
-trainData = [(170, 57, 32, W), (190, 95, 28, M), (150, 45, 35, W),
-(168, 65, 29, M), (175, 78, 26, M), (185, 90, 32, M), (171, 65, 28, W),
-(155, 48, 31, W), (165, 60, 27, W), (182, 80, 30, M), (175, 69, 28, W),
-(178, 80, 27, M), (160, 50, 31, W), (170, 72, 30, M)]
+trainData = [ ((170, 57, 32), W),
+((190, 95, 28), M),
+((150, 45, 35),  W),
+((168, 65, 29), M),
+((175, 78, 26), M),
+((185, 90, 32), M),
+((171, 65, 28), W),
+((155, 48, 31), W),
+((165, 60, 27), W),
+((182, 80, 30), M),
+((175, 69, 28), W),
+((178, 80, 27), M),
+((160, 50, 31), W),
+((170, 72, 30), M), ]
 
-testX = {(162, 53, 28, W), (168, 75, 32, M), (175, 70, 30, W), (180, 85, 29, M)}
+testX = [((155,40,35), W),
+ ((170,70,32), M),
+ ((175,70,35), W),
+ ((180,90,20),M)]
 
-testX_noAge = {(162, 53), (168, 75), (175, 70), (180, 85)}
+
+
 
 
 
@@ -27,23 +41,25 @@ from pprint import pprint
 # Find the min and max values for each column
 def dataset_minmax(dataset):
   minmax = list()
-  for i in range(len(dataset[0])-1):
-    col_values = [row[i] for row in dataset]
+  for col in range(len(dataset[0])):
+    col_values = [dataset[row][col] for row in range(len(dataset))]
     value_min = min(col_values)
     value_max = max(col_values)
     minmax.append([value_min, value_max])
-  pprint(minmax)
   return minmax
 
 # Rescale dataset columns to the range 0-1
 def normalize_dataset(dataset, minmax):
-  normList = list()
+  ndataset = list()
   for row in dataset:
-    for i in range(len(row)-1):
-      newrow = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
-    normList.append(newrow)
-  return normList
-
+    nrow = list()
+    for i in range(len(row)):
+      new_i = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+      if new_i < 0.01: new_i = 0.01
+      nrow.append(new_i)
+    ndataset.append(nrow)
+  #pdb.set_trace()
+  return ndataset
 
 def get_acc(groundtruth, prediction):
   correct = 0
@@ -52,50 +68,57 @@ def get_acc(groundtruth, prediction):
       correct += 1
   return correct / float(len(groundtruth)) * 100.0
 
-def sgd(XY, alpha, n):
-  pdb.set_trace()
-  theta = [0.0 for i in range(len(XY))]
+def sgd(X, Y, alpha, n):
+  theta = [0.0 for i in range(len(X[0]))]
   for _ in range(n):
-    for datum in XY:
-      Y_ = hypothesis(datum, theta)
-      e = datum[-1] - Y_
+    for i in range(len(X)):
+      Y_ = hypothesis(X[i], theta)
+      #pdb.set_trace()
+      e = Y[i] - Y_
       theta[0] = theta[0] + alpha * e * Y_ * (1.0 - Y_)
-      for i in range(len(datum)-1):
-        theta[i + 1] = theta[i + 1] + alpha * e * Y_ * (1.0 - Y_) * datum[i]
+      for m in range(len(X[i])-1):
+        #pdb.set_trace()
+        theta[m + 1] = theta[m + 1] + alpha * e * Y_ * (1.0 - Y_) * X[i][m]
   return theta
 
 def hypothesis(X, theta):
   y_ = theta[0]
   for i in range(len(X)-1):
     y_ += theta[i+1] * X[i]
-  return softmax(y_)
+  return logistic(y_)
 
-def softmax(y_):
+def logistic(y_):
   return 1.0/(1.0 + exp(-y_))
 
 def rsig(y_):
   return round(y_ * PRECISION)/PRECISION
 
-def prepare_data(data):
-
-  #pdb.set_trace()
-  return data
+def prepare_data(dataset):
+  X = list()
+  Y = list()
+  for xy in dataset:
+    X.append(list(xy[0]))
+    Y.append(xy[1])
+  for i in range(len(Y)):
+    if Y[i] == M:
+      Y[i] = 1
+    else: Y[i] = 0
+  return X, Y
 
 if __name__ == '__main__':
-  pdb.set_trace()
+
   n = 100
   alpha = .1
-  data = prepare_data(trainData)
-  XY = normalize_dataset(data, dataset_minmax(data))
-  Xt = testX
+  X, Y = prepare_data(trainData)
+  X = normalize_dataset(X, dataset_minmax(X))
+  Xt, Yt  = prepare_data(testX)
   predictions = list()
-  theta = sgd(XY, alpha, n)
+  theta = sgd(X, Y, alpha, n)
 
   for x in Xt:
     y_ = hypothesis(x, theta)
     y_ = rsig(y_)
     predictions.append(y_)
 
-  y = [xy[-1] for xy in XY]
-  acc = get_acc(y,y_)
+  acc = get_acc(Y,y_)
   print("acc: ", acc)
