2d1af11dd591b026d47c1f0090f4e7bd5db68b74
bardi <bardia.mojra@gmail.com>
2021-03-19 17:51:15 -0500

    Working on hw02

--- hw/hw02/svm.py
+++ hw/hw02/svm.py
@@ -2,6 +2,7 @@
 import pdb
 import numpy as np
 import matplotlib.pyplot as plt
+from random import random
 
 plt.style.use('ggplot')
 
@@ -14,9 +15,9 @@ D = [( 3,  4, -1),
      ( 4,  4, -1)]
 
 class svm:
-  def __init__(self, lr, lambda_param, iters, prt, log):
-    self.lr = lr
-    self.lp = lambda_param
+  def __init__(self, lr=0.01, lp, iters=1000, prt=False, log=False):
+    self.lr = lr # learning rate
+    self.lp = lp # lambda parameter
     self.iters = iters
     self.prt = prt
     self.w = None
@@ -24,9 +25,11 @@ class svm:
     if log==True:
       self.xHist = list()
       self.yHist = list()
-      self.yEstHist = list()
+      self.yEstHist =list()
       self.wHist = list()
       self.bHist = list()
+      self.wUpdHist = list()
+      self.bUpdHist = list()
       # add logs
     return
 
@@ -35,16 +38,56 @@ class svm:
     self.w = np.zeros(mfeatures)
     self.b = 0
 
-    for i in range(self.iters):
-      for idx, datum in enumerate(X):
-        condition = Y[idx] * (np.dot(datum, self.w) - self.b) >= 1
-        if condition==True:
+    XY = np.concatenate((X, Y), axis=1)
 
+    XY_tmp = np.ndarray.copy(XY)
+    for j in range(self.iters):
+      a = random.randint(0, len(XY_tmp)-1)
+      xDatum = XY_tmp[a][:-1]
+      yDatum = XY_tmp[a][-1]
+      XY_tmp = np.delete(XY_tmp, a, axis=0)
+      #pdb.set_trace()
+      yEst = yDatum * (np.dot(xDatum, self.w) - self.b) >= 1
+      if yEst: # binary classification only
+        wUpdate = self.lr * (2 * self.lp * self.w)
+        bUpdate = 0.0
+      else:
+        wUpdate = self.lr * (2 * self.lp * self.w - np.dot(xDatum, yDatum))
+        bUpdate = self.lr * yDatum
+      self.w -= wUpdate
+      self.b -= bUpdate
+      if len(XY_tmp)==0: XY_tmp = np.ndarray.copy(XY) # reload!!
+      if self.prt:
+        print('iter:{:3.d}'.format(j))
+      if self.log: # data logger!!
+        self.xHist.append(xDatum)
+        self.yHist.append(yDatum)
+        self.yEstHist.append(yEst)
+        self.wUpdHist.append(wUpdate)
+        self.bUpdHist.append(bUpdate)
+        self.wHist.append(self.w)
+        self.bHist.append(self.b)
     return
 
   def predict(self, X):
     linOutput = np.dot(X, self.w) - self.b
     return np.sign(linOutput)
+
+  def getHyperPlaneVal(self, x, w, b, offset):
+    res = (-w[0] * x + b + offset)/w[1]
+    return res
+
+  def visualize(self, X, Y):
+    fig = plt.figure()
+    plott = fig.add_subplot(1,1,1)
+    plt.scatter(X[:,0], X[:,1], marker='x', color='red', label='input')
+
+    x01 = np.amin(X[:,0])
+    x02 = np.amax(X[:,0])
+
+
+
+    return
 
 
 def get_data(data):
