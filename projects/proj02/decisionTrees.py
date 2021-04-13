b8c08ed20c3adecac6cb00881d1327c232fd2393
bardia <bardia.mojra@gmail.com>
2021-04-13 09:32:28 -0500

    Added lectures 09 and 10 on ensemble methods and unsupervised learning.

--- projects/proj02/decisionTrees.py
+++ projects/proj02/decisionTrees.py
@@ -3,3 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import operator
+
+https://medium.com/swlh/decision-tree-implementation-from-scratch-in-python-1cff4c00c71f
+
+
+read:
+https://towardsdatascience.com/entropy-how-decision-trees-make-decisions-2946b9c18c8
