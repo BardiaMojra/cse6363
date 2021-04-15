c9e146dc3a2469a9bba342ef266857d32c5206df
bardia <bardia.mojra@gmail.com>
2021-04-14 23:05:53 -0500

    saving progress.

--- projects/proj02/decisionTrees.py
+++ projects/proj02/decisionTrees.py
@@ -19,7 +19,8 @@ import pdb
 eps = np.finfo(float).eps
 
 class decisionTree:
-  def __init__(self, XYtrain, maxDepth):
+  def __init__(self, XYtrain, maxDepth=None):
+    self.depth = 0
     self.maxDepth = maxDepth
     self.X = XYtrain.drop(XYtrain.columns[-1], axis=1)
     self.Y = XYtrain[XYtrain.columns[-1]]
@@ -30,34 +31,57 @@ class decisionTree:
     self.df['Y'] = self.Y.copy()
 
 
-    pdb.set_trace()
     # build decision tree
-    self.tree = self.buildTree(self.df)
+    self.buildTree(self.df)
     self.printTree()
 
     return
 
   def buildTree(self, df, tree=None):
 
-    # determine which feature results in highest infoGain
+    # determine which input feature results in highest infoGain
     feature = self.getBestSplit(df)
-
+    print('feature: ', feature)
     # init tree
     if tree == None:
-      tree = list()
-      tree[feature] = list()
+      tree = dict()
+      tree[feature] = dict()
 
-    if df[feature].dtypes is not object:
+    print('\n\n')
+    print('df[feature].dtypes: ', df[feature].dtypes)
+    print('df[feature]: ', df[feature])
+
+    if df[feature].dtypes != object: # can add numerical dTree later
+      print('\n\n')
+      print('df[feature]: ', df[feature])
       print('\n>>>Err: non-object feature at ln:', i.getframeinfo(i.currentframe()).lineno)
       return
+    else: # only works with labels
+      for val in np.unique(df[feature]): # for each possible value in 'feature' col
+        df_ch = self.splitSamples(df, val, feature, opt.eq) # get child subset
+        Y_objs, cnts = np.unique(df_ch['Y'], return_counts=True) # return Y class cnts
 
-
-    for datum in df[feature]:
-      childDF = self.splitSamples(df, datum, feature, opt.eq)
-
+        if(len(cnts)==1): # single-class, pure group
+          tree[feature][val] = Y_objs[0]
+        else:
+          self.depth += 1
+          if self.maxDepth != None and self.depth >= self.maxDepth:
+            tree[feature][val] = Y_objs[np.argmax(cnts)]
+          else:
+            tree[feature][val] = self.buildTree(df_ch)
+    self.tree = tree
+    return
 
   def printTree(self):
+    print('\n\n')
+    print('-------------------- Decision Tree --------------------')
+    print(self.tree)
+    return
 
+  def splitSamples(self, df, val, col, _opt):
+    df_new = df[_opt(df[col], val)] # in df, all in 'col' w 'val' that satisfy '_opt' condition
+    df_new = df_new.reset_index(drop=True) # drop old index, reset to num index
+    return df_new
   def train(self, X, Y):
     self.inputFeatures = list()
 
@@ -65,31 +89,32 @@ class decisionTree:
     """Calculates total entropy of the give dataset.
     """
     totalEntropy = 0
-    for y in np.unique(data["y"]):
-      frac = data["y"].value_counts()[y] / len(data["y"])
+    for y in np.unique(data['Y']):
+      frac = data['Y'].value_counts()[y] / len(data['Y'])
       totalEntropy += -frac * np.log2(frac)
     return totalEntropy
 
   def getFeatureEntropy(self, data, a):
     """Calculates entropy per feature for a given dataset, H_D(Y|A).
     """
-    totalEntropy = 0
-    threshold = None # for numeric features
+    entropy = 0
+    #threshold = None # for numeric features
     if data[a].dtypes == object: # make sure datatype is what we expect
-      for featureValue in np.unique(data[a]):  # sum of H_D(Y|A=a)
+      for val in np.unique(data[a]):  # sum of H_D(Y|A=a)
         featureEntropy = 0
-        for y in np.unique(data["y"]):  # add all per datum feature entropies
-          num = len(data[a][data[a] == featureValue][data["y"] == y])
-          den = len(data[a][data[a] == featureValue])
+        for y in np.unique(data['Y']):  # add all per datum feature entropies
+          num = len(data[a][data[a] == val][data['Y'] == y])
+          den = len(data[a][data[a] == val])
           infoGain = num / (den + eps)  # information gain
           if infoGain > 0:
             featureEntropy += -infoGain * np.log2(infoGain)
-        featureWeight = len(data[a][data[a] == featureValue]) / len(data)
-        totalEntropy += featureWeight * featureEntropy
+        featureWeight = len(data[a][data[a] == val]) / len(data)
+        entropy += featureWeight * featureEntropy
     else: # else could be numeric data
       print('>>>Err: none object data at ln:', i.getframeinfo(i.currentframe()).lineno)
+    return entropy
 
-  def getBestSplit(self, data):
+  def getBestSplit(self, df):
     """
       For a given dataset, it return the feature with highest information gain.
       InfoGain = Entropy(data) - Sum of Entropy(data_subsets)
@@ -99,17 +124,16 @@ class decisionTree:
       Sum of all Entropy(data_subsets) =
     """
     infoGain = list()
-    thresholds = list()
-    parentEntropy = self.getTotalEntropy(data) # H_D(Y)
-
-    for a in list(data.columns[-1]):
-      featureEntropy, threshold = self.getFeatureEntropy(data, a) # H_D(Y\A=a)
+    parentEntropy = self.getTotalEntropy(df) # H_D(Y)
+    for a in list(df.columns[-1]):
+      featureEntropy = self.getFeatureEntropy(df, a) # H_D(Y\A=a)
       infoGain_a = parentEntropy - featureEntropy
       infoGain.append(infoGain_a)
-      thresholds.append(threshold)
-    return data.columns[:-1][np.argmax(infoGain)], thresholds[np.argmax(infoGain)]
+    return df.columns[:-1][np.argmax(infoGain)]
 
+  def _predict_target(self, feature_lookup, x, tree):
 
+  def predict(self, X):
 
 
 
