aad3a1aa27c403ac917012e01d4bc00a1dc9fdfb
bardia <bardia.mojra@gmail.com>
2021-05-13 18:08:05 -0500

    90% accuracy, done.

--- final_project/pca_02.py
+++ final_project/pca_02.py
@@ -19,31 +19,32 @@ translations = {"cane": "dog", "cavallo": "horse", "elefante": "elephant", \
 
 #animals=['dog', 'horse','elephant', 'butterfly', 'chicken', 'cat', 'cow', 'sheep']
 
-animals=['dog', 'horse', 'butterfly', 'chicken']
+animals=['dog', 'horse', 'sheep', 'chicken']
 
 def create_data_obj(src, subdir, categories, n_samples, img_size=250): # size, categories):
   print('---> Creating data object...')
   data = list()
+  print()
   for category, translate in categories.items():
     if translate in animals:
       path = src+subdir+category+'/'
       target = animals.index(translate)
-      print()
-      print('   ----------> at %s , %s is a class' % (path, translate))
+      print('---> at %s, %s is a class' % (path, translate))
       for i, file in enumerate(os.listdir(path)):
         if i < n_samples:
           try:
             fname, ext = os.path.splitext(file)
-            if prt is True:
+            #if prt is True:
               #print()
-              print('- %5d  %s%s' % (i,fname,ext))
+              #print(' - %5d  %s%s' % (i,fname,ext))
             image = Image.open(path+fname+ext)
             #image = change_contrast(image, 95) # change contrast for grey scale
             image = ImageOps.grayscale(image)
             image = image.resize((img_size,img_size))
+            #set_trace()
             pixels = np.asarray(image, order='f') # write to memory fortran style
             pixels = pixels.astype('float32')
-            data.append(np.asarray([i,pixels,target,src,category,translate,fname+ext]))
+            data.append(np.asarray([i,pixels,target,src,category,translate,fname+ext], dtype=object))
           except OSError as error: # if returns error
             print('--> cant read image %s already exists.' % path+fname+ext)
             pass
@@ -105,48 +106,58 @@ def get_data(data, label=None):
   Y = np.asarray(Y, order='f') # write to memory fortran style
   return X,Y
 
-def pca(X):
+def pca(X, components):
   print('---> Computing principal components...')
   assert X.shape[0]!=0, '   |---> Err: selected dataset is emptry...'
   # get dim
-  n_samples, dim = X.shape
-  # center data
-  mean = X.mean(axis=0)
-  std = np.std(X, axis=0)
-  X = (X - mean)/std
-  use_compact_technique = False # disabled, was getting poor results
-  if dim > n_samples and use_compact_technique==True:
-    # use compact trick
-    covar = np.dot(X,X.T) # calculate covariance matrix
-    eVals, eVecs = np.linalg.eigh(covar) # get eigen values and vectors of covariance
-    compact = np.dot(X.T,eVecs).T # compact trick
-    V = compact[::-1] # get last eigenvectors (highest value)
-    S = np.sqrt(eVals)[::-1] # get last eigenvalues (highest value)
-    for i in range(V.shape[1]):
-      V[:,i] /= S  # projection (V) divided by variance (S)
-  else:
-    # normal method, use SVD
-    U,S,V = np.linalg.svd(X)
-    V = V[:n_samples] # the rest is not usefull information
-  return V, S, mean
+  #n_samples, dim = X.shape
+  #print('   |---> X.shape: ', X.shape)
 
-def plot_utility(label, image_size, V, img_mean, b, saveImg=True):
+  mean = X.mean(axis=0) # normalize data
+  std = np.std(X, axis=0) + 0.001 # increase std for potential noise
+  Xm = (X - mean)/std
+  U,S,V = np.linalg.svd(Xm, full_matrices=True)
+  #set_trace()
+  V = V[:,:components] # the rest is not usefull information
+  #print('   |---> V.shape: ', V.shape)
+  #print('   |---> Xm.shape: ', Xm.shape)
+  Z = Xm.dot(V)
+  mean = Z.mean(axis=0) # normalize PCA data
+  return V, S, mean, Z
+
+def plot_utility(label, image_size, Z, img_mean, b, numPC, saveImg=True):
   # show images
   fig,axs = plt.subplots(2,5, sharex=True, sharey=True)
-  fig.suptitle("Eigen Mean & Features - "+label)
-  axs[0,0].imshow(img_mean.reshape(image_size,image_size))
-  axs[0,0].set_title('Proj. Mean')
-  for i in range(9):
+  fig.suptitle('PC Features ({}) - {}, Img:{}x{}'.format(numPC,label,image_size,image_size))
+  #img = np.expand_dims(img_mean,axis=1)
+  #axs[0,0].imshow(img.reshape(image_size,image_size))
+  #axs[0,0].set_title('PC mean')
+  for i in range(10):
     #plt.subplot(2,5,i+2)
-    axs[int((i+1)/5),(i+1)%5].set_title('feat. n°{}'.format(i+1))
-    axs[int((i+1)/5),(i+1)%5].imshow(V[i].reshape(image_size,image_size))
-  figname = './output/figure_{}_{}.png'.format(b,label)
+    axs[int((i)/5),(i)%5].set_title('PC n°{}'.format(i+1))
+    axs[int((i)/5),(i)%5].imshow(Z[i].reshape(image_size,image_size))
+  figname = './output/figure_{}_{}_w_{}_PC_{}x{}'.format(b,label,numPC,image_size,image_size)
   if saveImg==True:
     plt.savefig(figname, bbox_inches='tight',dpi=300)
-    print('---> Saving class eigen projections figure: '+figname)
-
+    print('---> Saving class PC projections figure: '+figname)
   #fig.show(bbox_inches='tight',dpi=100)
   return
+
+def plot_PCvariance(label, image_size, b, saveImg=True):
+  with plt.style.context('seaborn'):
+    plt.figure()
+    plt.title('Principle Component Variances: {}, Img:{}x{}'.format(label,image_size,image_size))
+    plt.xlabel('Principle Components')
+    plt.ylabel('Variance')
+    plt.plot(S)
+    #plt.legend('')
+    figname = './output/figure_PCvar_{}_{}_img_{}x{}'.format(b,label,image_size,image_size)
+    if saveImg==True:
+      plt.savefig(figname, bbox_inches='tight',dpi=300)
+      print('---> Saving class PC variance figure: '+figname)
+    #fig.show(bbox_inches='tight',dpi=100)
+  return
+
 
 class knn_classifier:
   def __init__(self, trainXY, testXY=None, k=1, precision=4, prt=True, vmode='simple', labels=None):
@@ -172,65 +183,80 @@ class knn_classifier:
 
   def euclidean_dist(self, A, B):
     res = np.sqrt(np.sum((A-B)**2, axis=1))
+    #set_trace()
     return res
 
   # Calculate nearest neighbors
   def get_kneighbors(self, testX, dist_mode=False): # trainX, trainX_i, # of nearest neighbors
     print('---> Computing distance to K nearest neighbors...')
+    print_all = False
+    dist = list()
     knn_dists = list()
     knn_ids = list()
+    knn_cls_ids = list()
     k = self.k
-    set_trace() # check
+    #set_trace() # check
     # compute distances for given test set
-    dist = [self.euclidean_dist(testx_i, self.trainX) for testx_i in testX]
+    for testx_i in testX:
+      testx = np.expand_dims(testx_i, axis=0)
+      #print('testx: {}'.format(testx.shape))
+      d = self.euclidean_dist(testx, self.trainX)
+      #set_trace() # check
+      dist.append(d)
     for row in dist: # sort distances
+      #set_trace() # check
       index = enumerate(row)
       sorted_knn = sorted(index, key=lambda x: x[1])[:k] # sort tuples based on 1th element
       idList = [tup[0] for tup in sorted_knn]
       distList = [tup[1] for tup in sorted_knn]
+      gtList = [ self.trainY[tup[0]] for tup in sorted_knn]
       knn_dists.append(distList)
       knn_ids.append(idList)
+      knn_cls_ids.append(gtList)
+      if print_all:# NBUG
+        print('idList: {}'.format(str(idList)))
+        print('distList: {}'.format(str([round(i,self.precision) for i in distList])))
+        print('gtList: {}'.format(str(gtList)))
+    knn_ids = np.asarray(knn_ids)
+    knn_dists = np.asarray(knn_dists)
+    knn_cls_ids = np.asarray(knn_cls_ids)
     #set_trace() # check
-    if self.prt == True: # print log
-      print('')
-      print(' TestX ID | KNN IDs (trainXY)        |')
-      print('__________|__________________________|')
-      for i in range(len(knn_ids)):
-        #diststr =' '.join([str(round(item,6)) for item in knn_dists[i] ])
-        idsstr =', '.join([str(round(item)) for item in knn_ids[i] ])
-        print(' {:5d}  |  {}  |'.format(i+1 , idsstr))
-    print()
+    if self.prt == True:
+      self.print_neighbors(knn_ids, knn_cls_ids)
     if dist_mode:
-      return np.array(knn_dists), np.array(knn_ids)
+      return knn_dists, knn_ids
     else:
-      return np.array(knn_ids)
+      return knn_ids
 
   def predict(self, testX, vmode='simple'):
     ''' prediction modes:
           - simple: simple voting among K nearest neighbors.
           - weighted: uses distances as weights in voting process.
     '''
-    prt = self.prt
+    prt = False
     print('---> Predicting class based on KNN: in {} mode...'.format(vmode))
     if vmode=='weighted':
       probs = list()
       dists, ids = self.get_kneighbors(testX, dist_mode=True)
       inv_dists = 1/dists
-      mean_invdist = inv_dists / np.sum(inv_dists, axis=1)[:, np.newaxis]
+      m_inv_dists = inv_dists / np.sum(inv_dists, axis=1)[:, np.newaxis]
       #set_trace()
-      i = 1
-      for xdatum in mean_invdist:
-        Y = self.trainY[ids[i]]
-        for label in range(len(animals)):
-          ids = np.where(Y==label)
-          prob_ids = np.sum(xdatum[ids])
-          probs.append(np.array(prob_ids))
-        i += 1
-      yest_probs = np.array(probs).reshape(testX.shape[0], len(animals))
+      for i, row in enumerate(m_inv_dists):
+        row_est = self.trainY[ids[i]]
+        #print('row_est: {}'.format(str(row_est)))
+        #set_trace()
+        for v in np.arange(0, len(self.labels)):
+          indx = np.where(v==row_est)
+          prob_idx = np.sum(row[indx])
+          probs.append(np.array(prob_idx))
+          #set_trace()
+          #if prt:# NBUG
+            #print('indx: {}'.format(str(indx)))
+            #print('prob_idx: {}'.format(str(prob_idx)))
+            #print('probs: {}'.format(str(probs)))
+      yest_probs = np.array(probs).reshape(testX.shape[0], len(self.labels))
       yest = np.array([np.argmax(p) for p in yest_probs])
-      if prt:
-        print('--> Predictions: ')
-        print(yest)
+      #set_trace()
     else: # in simple mode
       neighbors = self.get_kneighbors(testX)
       listtemp = list()
@@ -239,20 +265,56 @@ class knn_classifier:
         tmp = np.argmax(np.bincount(self.trainY[n]))
         listtemp.append(tmp)
       yest = np.array(listtemp)
+    print()
+    if prt:
+      print('---> Set predictions: ')
+      print(yest)
+    print('---------->>>')
     return yest
 
   def get_acc(self, testX, testY, vmode='simple'):
-    if vmode == 'both': # returns simple yest
-      self.prt=True # print outputs
-      yest = self.predict(testX, vmode='simple')
-      yest_w = self.predict(testX, vmode='weighted')
-    else:
-      yest = self.predict(testX, vmode=vmode)
+    yest = self.predict(testX, vmode=vmode)
+    #set_trace()
     acc = float(sum(yest==testY)) / float(len(testY))
-    if self.prt:
-      print('--->> Test set prediction accuracy: {}'.format(acc))
+    self.print_est(testX, testY, yest, vmode, acc)
     return acc
-  # end of knn_classifier class
+
+  def print_neighbors(self, knn_ids, knn_cls_ids, lim=True, limVal=10):
+    print('')
+    print('  | TestX ID | KNN IDs (Class IDs) - trainXY                        |')
+    print('  |__________|______________________________________________________|')
+    rows, cols = knn_ids.shape
+    if lim and (rows>limVal): rows=limVal
+    for i in range(0, rows):
+      string = ''
+      for j in range(0, cols):
+        nstring = '{:4d}({:1d}) '.format(knn_ids[i,j], knn_cls_ids[i,j])
+        string = string + nstring
+      print('    {:4d}     | {}   '.format(i+1 , string))
+    if lim: print('     ...     |  ...   ')
+    print()
+    return
+
+  def print_est(self, testX, testY, Yest, vmode, acc, lim=True, limVal=10, ):
+    print('---> Printing test results, in {} mode...'.format(vmode))
+    print()
+    print('  | TestX ID |   Y    |  Yest  |')
+    print('  |__________|________|________|')
+    rows =testX.shape[0]
+    if lim and (rows>limVal): rows=limVal
+    for x in range(0, rows):
+      print('    {:4d}     | {:4d}   | {:4d} '.format(x+1 , testY[x].astype(int), Yest[x]))
+    if lim: print('      ...    |   ...  |   ... ')
+    print()
+    print('   \-------->>> Prediction accuracy ({}): {:.3f}%  <<-|'.format(vmode,(acc*100)))
+    print('\n')
+    return
+  # end of knn_classifier class ---------------------
+
+
+
+
+
 
 if __name__ == '__main__':
   ''' ToDo: add a auto downloader
@@ -289,11 +351,12 @@ if __name__ == '__main__':
   ''' run config
   '''
   prt = True
-  image_size = 30 # pixels, equal height and width
-  samples_per_class = 100
+  image_size = 48 # pixels, equal height and width
+  samples_per_class = 300
   shuffles = 10 # shuffle n times to mix data
   testfrac = .2 # 0-1.0 | test set fraction of main set
-
+  PC_ratio = 60 # %
+  numPC = int((PC_ratio/100)*samples_per_class)
 
   ''' create data object
     data.append(np.asarray([i,pixels,target,src,category,translate,fname+ext]))
@@ -318,34 +381,37 @@ if __name__ == '__main__':
   if skip_process_dataXY == False:
     dataXY = list()
     mfeat = image_size**2  # = img_size * img_size + y
-    keysXY = np.full((0, mfeat+ 1), 1)
-    keyy = np.full((1, mfeat+ 1), 1)
+    #keysXY = np.full((0, mfeat+ 1), 1)
+    #keyy = np.full((1, mfeat+ 1), 1)
     labels = list()
     eigenXY_class = np.empty((samples_per_class, mfeat+ 1))
-    eigenXY = np.empty((0, mfeat+ 1))
+    eigenXY = np.empty((0, numPC+ 1))
     print()
     # get data per class and process to obtain PCA
     for b, (directory, label) in enumerate(translations.items()):
       if label in animals:
         X, Y = get_data(data, label=label)
-        #nSamps, mFeat = len(X), len(X[0])
-        V,S,img_mean = pca(X)
+        V,S,img_mean, Z = pca(X, numPC)
         #set_trace()
         dataXY.append([label,X,Y,V,S,img_mean])
         labels.append(label)
-        key = np.reshape(img_mean, (1,-1))
-        y = np.full((1,1), Y[0])
+        #key = np.reshape(img_mean, (1,-1))
         # copy processed PCA keys - mean
-        keyy = copy.deepcopy(np.concatenate((key,y), axis=1))
-        keysXY = copy.deepcopy(np.concatenate((keysXY, keyy), axis=0))
         #set_trace()
         Y = np.expand_dims(Y,axis=1)
-        eigenXY_class = copy.deepcopy(np.concatenate((V,Y), axis=1))
+        # new data set
+        #print('   |---> Y.shape: ', Y.shape)
+        #print('   |---> Z.shape: ', Z.shape)
+        eigenXY_class = copy.deepcopy(np.concatenate((Z,Y), axis=1))
         eigenXY = copy.deepcopy(np.concatenate((eigenXY,eigenXY_class), axis=0))
         #print(eigenXY)
         #set_trace()
         # plot or save images
-        plot_utility(label, image_size, V, img_mean, b)
+        #print('   |---> V.shape: ', V.shape)
+        eImgs = Z.dot(V.T)
+        #print('   |---> eImgs.shape: ', eImgs.shape)
+        plot_utility(label, image_size, eImgs, img_mean, b, numPC)
+        plot_PCvariance(label, image_size, b)
 
     # save processed dataset object
     print('---> Saving dataXY object to binary file...')
@@ -353,9 +419,9 @@ if __name__ == '__main__':
       pkl.dump(dataXY, dataXY_file)
 
     # save keysXY dataset object
-    print('---> Saving keysXY object to binary file...')
-    with open('keysXY.json', 'wb') as keysXY_file:
-      pkl.dump(keysXY, keysXY_file)
+    #print('---> Saving keysXY object to binary file...')
+    #with open('keysXY.json', 'wb') as keysXY_file:
+      #pkl.dump(keysXY, keysXY_file)
 
     # save eigenXY dataset object
     print('---> Saving eigenXY object to binary file...')
@@ -375,9 +441,9 @@ if __name__ == '__main__':
         dataXY = pkl.load(dataXY_file)
 
       # load keysXY dataset object
-      print('---> Loading keysXY object from binary file...')
-      with open('keysXY.json', 'rb') as keysXY_file:
-        keysXY = pkl.load(keysXY_file)
+      #print('---> Loading keysXY object from binary file...')
+      #with open('keysXY.json', 'rb') as keysXY_file:
+        #keysXY = pkl.load(keysXY_file)
     else:
       # load eigenXY dataset object
       print('---> Loading eigenXY object from binary file...')
@@ -390,8 +456,6 @@ if __name__ == '__main__':
         labels = pkl.load(labels_file)
 
 
-  ''' print labels
-  '''
   print()
   print('---> Labels and class indices:')
   for i, label in enumerate(labels):
@@ -415,8 +479,18 @@ if __name__ == '__main__':
   print('   |---> PCA image test set shape: {}'.format(testXY.shape))
 
   #set_trace()self, trainXY, testXY=None, k=1, precision=4, prt=True, vmode='simple', labels=None):
-  knn = knn_classifier(trainXY, testXY, k=5, prt=True, vmode='weighted', labels=labels)
+  knn = knn_classifier(trainXY, testXY, k=7, prt=True, vmode='weighted', labels=labels)
+
+  print()
+  print('\------> Test model with: training data')
+  knn.get_acc(knn.trainX, knn.trainY, vmode='weighted')
+  knn.get_acc(knn.trainX, knn.trainY, vmode='simple')
+
+
+  print('\------> Test model with: test data')
   knn.get_acc(knn.testX, knn.testY, vmode='weighted')
+  knn.get_acc(knn.testX, knn.testY, vmode='simple')
+
   print('\n---> End of process.')
 
 
